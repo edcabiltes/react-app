1010ef650441e07533c615aefd295d027b2601f5
Ed Cabiltes <edcabiltes@outlook.com>
2025-01-31 15:15:30 +0800

    Add click event

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,17 @@
 const Home = () => {
-    
+  const handleClick = (e) => {
+    console.log('hello guys!', e)
+  }
+
+  const handleClickAgain = (name, e) => {
+    console.log('hello ' + name, e.target)
+  }
+
   return (
     <div className="home">
         <h2>Homepage</h2>
+        <button onClick={handleClick} >Click me</button>
+        <button onClick={(e) => handleClickAgain('ed', e)} >Click me again</button>
     </div>
   )
 }
