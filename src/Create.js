927c8664b5da891aa6ec7203b50c3ea22d0df6c6
Ed Cabiltes <edcabiltes@outlook.com>
2025-02-05 17:06:20 +0800

    Added redirect after POST request

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('ed');
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,6 +17,7 @@ const Create = () => {
       body: JSON.stringify(blog)
     }).then(() => {
       console.log('new blog added');
+      navigate('/');
     })
   }
 
